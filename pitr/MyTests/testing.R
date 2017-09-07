1a228c1cc38d08665099e5f9b13d2d3ecba5cc54
Dave Fifield <dave.fifield@canada.ca>
2017-09-07 14:57:57 -0230

    - name change

--- pitr/MyTests/testing.R
+++ pitr/MyTests/testing.R
@@ -6,5 +6,5 @@ ch <- pitdb_open("C:/Users/fifieldd/Documents/Offline/Projects/Burrow_Logger/Dat
 
 sp_tags <- RODBC::sqlFetch(ch, "lkpSpecialTags", as.is = T)
 filename <-  "C:/Users/fifieldd/Documents/Offline/Projects/Burrow_Logger/Data/2017/Downloads/gipi1_report_2017_08_08_13_00_09.txt"
-ret <- pitdb_process_bird_report_file(filename, sp_tags = sp_tags, verbose = T)
+ret <- pitdb_parse_bird_report_file(filename, verbose = T)
 RODBC::odbcClose(ch)
