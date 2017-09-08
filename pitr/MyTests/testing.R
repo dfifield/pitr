ad27ba8212945e167afaf5e7e506a08844256b94
Dave Fifield <dave.fifield@canada.ca>
2017-09-08 16:02:48 -0230

    testing queries

--- pitr/MyTests/testing.R
+++ pitr/MyTests/testing.R
@@ -1,10 +1,15 @@
 library(pitr)
 library(RODBC)
 
-#ch <- pitdb_open("C:/Users/fifieldd/Documents/Offline/Projects/Burrow_Logger/Software/pitr/R/testdb.accdb")
-ch <- pitdb_open("C:/Users/fifieldd/Documents/Offline/Projects/Burrow_Logger/Data/Burrow logger master0.1.accdb")
+ch <- pitdb_open("C:/Users/fifieldd/Documents/Offline/Projects/Burrow_Logger/Software/pitr/tests/testdb.accdb")
+#ch <- pitdb_open("C:/Users/fifieldd/Documents/Offline/Projects/Burrow_Logger/Data/Burrow logger master0.1.accdb")
 
-sp_tags <- RODBC::sqlFetch(ch, "lkpSpecialTags", as.is = T)
 filename <-  "C:/Users/fifieldd/Documents/Offline/Projects/Burrow_Logger/Data/2017/Downloads/gipi1_report_2017_08_08_13_00_09.txt"
 ret <- pitdb_parse_bird_report_file(filename, verbose = T)
+pitdb_summarize_parsed_file(ret, ch = ch, verbose = F)
+
+
+
+sqlQuery(ch, "INSERT INTO tblImports ( [DateTime], FetchType ) SELECT #9/8/2017# AS Expr1, \"fourth\" AS EXPR2, 2 AS Expr3')
+
 RODBC::odbcClose(ch)
