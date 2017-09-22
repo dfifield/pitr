e55c21a441785f4432a962cf1714f44b47e8d931
Dave Fifield <dave.fifield@canada.ca>
2017-09-22 16:17:57 -0230

    update

--- pitr/MyTests/testing.R
+++ pitr/MyTests/testing.R
@@ -1,15 +1,19 @@
 library(pitr)
 library(RODBC)
 
-ch <- pitdb_open("C:/Users/fifieldd/Documents/Offline/Projects/Burrow_Logger/Software/pitr/tests/testdb.accdb")
-#ch <- pitdb_open("C:/Users/fifieldd/Documents/Offline/Projects/Burrow_Logger/Data/Burrow logger master0.1.accdb")
+data_sets <- "C:/Users/fifieldd/Documents/Offline/Projects/Burrow_Logger/Data/2017/Downloads"
 
-filename <-  "C:/Users/fifieldd/Documents/Offline/Projects/Burrow_Logger/Data/2017/Downloads/gipi1_report_2017_08_08_13_00_09.txt"
-ret <- pitdb_parse_bird_report_file(filename, verbose = T)
-pitdb_summarize_parsed_file(ret, ch = ch, verbose = F)
+files = c("gipi1_report_2017_08_28_13_00_03.txt", "gipi2_report_2017_08_28_13_00_04.txt", "gipi3_report_2017_08_28_13_00_04.txt", "gipi4_report_2017_08_28_13_00_02.txt", "gipi5_report_2017_08_28_13_00_16.txt")
+files = file.path(data_sets, files)
+
+#ch <- pitdb_open("C:/Users/fifieldd/Documents/Offline/Projects/Burrow_Logger/Software/pitr/tests/testdb.accdb")
+mych <- pitdb_open("C:/Users/fifieldd/Documents/Offline/Projects/Burrow_Logger/Data/Burrow logger master0.1.accdb")
+
+files %>%
+  walk(function(.x, ch = mych) {pitdb_parse_bird_report_file(.x, verbose = T) %>% pitdb_summarize_parsed_file(ch = ch, verbose = F)})
 
 
+sqlQuery(ch, "INSERT INTO tblImports ( [DateTime], Notes, FetchType ) SELECT #9/8/2017# AS Expr1, 'fourth' AS EXPR2, 2 AS Expr3")
+res <- sqlQuery(ch, "SELECT @@IDENTITY")
 
-sqlQuery(ch, "INSERT INTO tblImports ( [DateTime], FetchType ) SELECT #9/8/2017# AS Expr1, \"fourth\" AS EXPR2, 2 AS Expr3')
-
-RODBC::odbcClose(ch)
+pitdb_close(ch)
