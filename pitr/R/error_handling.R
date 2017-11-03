deaad39cf7a7938e590986e262fa997c1e01813a
Dave Fifield <dave.fifield@canada.ca>
2017-11-03 10:22:58 -0230

    - error functions for nest attempt import

--- pitr/R/error_handling.R
+++ pitr/R/error_handling.R
@@ -61,6 +61,25 @@ file_already_imported <- function(e) {
   invisible(.)
 }
 
+no_nest_attmpt <- function(e) {
+  warning("Could not find nesting attempt for this burrow in this year so chick ID will not be filled in.", immediate. = T)
+}
+
+# print warning if bird already exists in a nest attempt this year in some other burrow
+warn_other_nest_attmpt <- function(e) {
+  # fetch the data
+  . <- get(".", parent.frame())
+
+  #dat <- paste0(., collapse = ", ")
+  mess <- sprintf(paste0("\nThis bird is already involved in a nest attempt in %d in burrow %s involving %d (Bird1ID) and %d (Bird2ID)\n",
+              "Failing to create nest attempt for this bird\n"),
+                  .$Yr, .$BurrowID, .$Bird1, .$Bird2)
+
+  warning(mess, immediate. = TRUE)
+  invisible(.)
+}
+
+
 ensure_data_is_returned <- ensurer::ensures_that(is.data.frame(.), fail_with = sql_failure)
 ensure_insert_success <- ensurer::ensures_that(length(.) == 0, fail_with = sql_failure)
 ensure_one_row_returned <- ensurer::ensures_that(nrow(.) == 1, fail_with = too_many_rows)
@@ -68,3 +87,7 @@ warn_insert_success <- ensurer::ensures_that(length(.) == 0, fail_with = sql_war
 warn_tag_reads_not_inserted <- ensurer::ensures_that(nrow(.) == 0, fail_with = tag_reads_not_inserted_warn)
 warn_non_reporting <- ensurer::ensures_that(length(.) == 0, fail_with = boards_not_reporting_warn)
 ensure_not_already_imported <- ensurer::ensures_that(nrow(.) == 0, fail_with = file_already_imported)
+warn_no_nest_attempt <- ensurer::ensures_that(nrow(.) == 1, fail_with = no_nest_attmpt)
+warn_no_other_nest_attempt <- ensurer::ensures_that(nrow(.) == 0, fail_with = warn_other_nest_attmpt)
+
+
