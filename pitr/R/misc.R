9097357b248ab98116ed9dae32f8bf31ab393a95
Dave Fifield <dave.fifield@canada.ca>
2018-07-25 16:10:01 -0230

    - utility fnc to print out downloaded records

--- pitr/R/misc.R
+++ pitr/R/misc.R
@@ -111,3 +111,11 @@ per_dataclass_filter <- function(df, mydepl) {
 }
 
 
+# print out records from a download file with the name of the record type (tag_read, status, etc) if recs is not empty or NULL
+print_recs <- function(recs, nm) {
+  if(is.not.null(recs) && nrow(recs) > 0) {
+    cat(sprintf("\n%s:\n", nm))
+    print(recs, n = nrow(recs))
+    cat("\n")
+  }
+}
