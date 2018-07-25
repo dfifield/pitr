c587f9e4e9ffa42591bfe531ae795d82524b5538
Dave Fifield <dave.fifield@canada.ca>
2018-07-25 16:10:27 -0230

    comment out pseudo code to make compiler happy

--- pitr/R/process_data_downloads.R
+++ pitr/R/process_data_downloads.R
@@ -25,33 +25,33 @@ pitdb_process_data_downloads <- function(db = NULL, path = NULL, start_time = XX
   !is.null(db) || stop("Parameter db is missing from pitdb_process_data_downloads.")
   !is.null(path) || stop("Parameter path is missing from pitdb_process_data_downloads.")
 
-
-  # endlessly process files.
-  while(True) {
-    # any new files to process? How to determine?
-    #
-    # build a list of filenames and modification times of all files ever processed. ... no, that's just duplicating what's in tblImports.
-    # could just check each file to see if it's in tblImports. Grab a new copy of tblImports each time we wake up and go from there...
-    # # Check if file already imorted. Rudimentary check which only considers filename
-
-    # here's some similar code from load_file.R....
-    # tblImports <- RODBC::sqlFetch(ch, "tblImports", as.is = T) %>%
-    #   ensure_data_is_returned
-    #
-    # # Check if filename in list of imported files with or without full path.
-    # res <- if (compare_full_pathname) {
-    #     dplyr::filter(tblImports, Filename == filename)
-    #   } else {
-    #     dplyr::filter(tblImports, basename(Filename) == basename(filename))
-    #   }
-    #
-    # # Print warning (and quit) if nrow(res) is not 0, ie if the file has already been imported
-    # ensure_not_already_imported(res) %>% nrow() == 0 || return(FALSE)
-
-    if (any_new_files to process) {
-      # duplicate functionality of ...\software\data processing\R\Process download data.R that is currently called by Outlook from VBA.
-    } else {
-      # sleep for until next check
-    }
-  }
+#
+#   # endlessly process files.
+#   while(True) {
+#     # any new files to process? How to determine?
+#     #
+#     # build a list of filenames and modification times of all files ever processed. ... no, that's just duplicating what's in tblImports.
+#     # could just check each file to see if it's in tblImports. Grab a new copy of tblImports each time we wake up and go from there...
+#     # # Check if file already imorted. Rudimentary check which only considers filename
+#
+#     # here's some similar code from load_file.R....
+#     # tblImports <- RODBC::sqlFetch(ch, "tblImports", as.is = T) %>%
+#     #   ensure_data_is_returned
+#     #
+#     # # Check if filename in list of imported files with or without full path.
+#     # res <- if (compare_full_pathname) {
+#     #     dplyr::filter(tblImports, Filename == filename)
+#     #   } else {
+#     #     dplyr::filter(tblImports, basename(Filename) == basename(filename))
+#     #   }
+#     #
+#     # # Print warning (and quit) if nrow(res) is not 0, ie if the file has already been imported
+#     # ensure_not_already_imported(res) %>% nrow() == 0 || return(FALSE)
+#
+#     if (any_new_files to process) {
+#       # duplicate functionality of ...\software\data processing\R\Process download data.R that is currently called by Outlook from VBA.
+#     } else {
+#       # sleep for until next check
+#     }
+#   }
 }
