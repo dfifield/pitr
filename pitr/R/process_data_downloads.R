75e982c5fa1840d4fad59f7fb70e0884aa8229ad
Dave Fifield <dave.fifield@canada.ca>
2018-08-02 11:48:15 -0230

    add logging and fix processing logic

--- pitr/R/process_data_downloads.R
+++ pitr/R/process_data_downloads.R
@@ -1,3 +1,4 @@
+#' @importFrom magrittr %>%
 #'@export
 #'@title Process downloaded PIT tag data
 #'
@@ -7,63 +8,107 @@
 #'@param path The pathname to the folder where data files wait to be processed. Usually placed there my Outlook or some other mailer. Use C:/foo/bar/blah Unix forward
 #'   slash notaion.
 #'@param report_path Full pathname to folder where knitted import reports are to be stored. Defaults to \code{path/../Import records}
+#'@param logfile_path Full pathname of file to receive logging information. Defaults to \code{dirname(db)/import_log.txt}.
+#'@param log_level One of \code{logging::loglevels}. E.g. \code{ c("NOTSET", "FINEST", "FINER", "FINE", "DEBUG", "INFO", "WARN", "ERROR", "CRITICAL", "FATAL") }
+#'   Default is \code{"INFO"}
 #'@param compare_full_pathanme (Default \code{FALSE}). If \code{TRUE} then comparing filenames in \code{path} to those already imported into \code{db} (in tblImports)
 #'   only considers the filename and ignores the full pathname.
 #'@param start_time Time of day to start looking for new files. Useful to avoid wasting time looking for new files during parts of the day when they
-#'   are not expected to arrive. Default \code{12:00:00 noon}
+#'   are not expected to arrive. Default \code{14:00:00 noon}
 #'@param end_time Time of day to stop looking for new files. Useful to avoid wasting time looking for new files during parts of the day when they
-#'   are not expected to arrive. Default \code{14:00:00}
+#'   are not expected to arrive. Default \code{14:30:00}
 #'@param sleep_time Seconds between checks for new files. Default 30 * 60 (ie. 30 minutes).
 
 #'@details
 #'
-#'@return Doesn't normally return.
+#'@return Doesn't normally return, but -1 on error.
 #'@section Author: Dave Fifield
-pitdb_process_data_downloads <- function(db = NULL, path = NULL, report_path = NULL, compare_full_pathname = FALSE, start_time = NULL, end_time = NULL, sleep_time = 30 * 60 ){
+pitdb_process_data_downloads <- function(db = NULL,
+                                         path = NULL,
+                                         report_path = NULL,
+                                         logfile_path = NULL,
+                                         log_level = "INFO",
+                                         compare_full_pathname = FALSE,
+                                         start_time = NULL,
+                                         end_time = NULL,
+                                         sleep_time = 30 * 60 ){
 
+
+  logging::basicConfig(level = log_level)
+  if (!is.null(logfile_path)){
+    logging::addHandler(logging::writeToFile, file = logfile_path, level = log_level)
+  } else if (!is.null(db)) {
+    logging::addHandler(logging::writeToFile, file = file.path(dirname(db), "import_log.txt"), level='DEBUG')
+  } # else only logging to console....
+
+  logging::loginfo("Starting import server....pid = %d", Sys.getpid())
 
   # Check args
-  !is.null(db) || stop("Parameter db is missing from pitdb_process_data_downloads.")
-  !is.null(path) || stop("Parameter path is missing from pitdb_process_data_downloads.")
+  if (is.null(db)) {
+    logging::logerror("Parameter db is missing from pitdb_process_data_downloads.")
+    logging::loginfo("Shutting down import server.")
+    return(-1)
+  }
 
-  !is.null(report_path) || (report_path <- file.path(dirname(path), "Import records"))
+  if (is.null(path)) {
+    logging::logerror("Parameter path is missing from pitdb_process_data_downloads.")
+    logging::loginfo("Shutting down import server.")
+    return(-1)
+  }
+
+  if(is.null(report_path))
+    report_path <- file.path(dirname(path), "Import records")
+
+  # get initial snapshot of existing files in download folder and keep only ".txt" files
+  prev <- fileSnapshot(path)
+  new_files <- filter_unwanted(rownames(prev$info), db, compare_full_pathname)
+  is.character(new_files) || return(-1)
+
+  logging::logdebug("New files: %s", new_files)
+
+  # Process new_files (if any), sleep, check for new files, process new files, ad nauseum...
+  while(TRUE) {
+    if (length(new_files) != 0) {
+      logging::loginfo("Processing new files: %s", paste(new_files, collapse = ", "))
+      do_import(db = db, files = file.path(prev$path, new_files, fsep = "\\"), report_path = report_path)
+    }
+
+    # sleep until next check
+    logging::logdebug("Sleeping until %s (%d secs)...", lubridate::now() + sleep_time, sleep_time)
+    Sys.sleep(sleep_time)
+    logging::logdebug("Yawn... waking up at %s", lubridate::now())
+
+    # Get a new snapshot and compare
+    curr <- fileSnapshot(path)
+    new_files <- filter_unwanted(changedFiles(prev, curr)$added, db, compare_full_pathname)
+    is.character(new_files) || return(-1)
+    logging::logdebug("New files: %s", new_files)
+    prev <- curr
+
+    # keep tidy. necessary?
+    gc()
+ }
+}
+
+filter_unwanted <- function(files, db, compare_full_pathname){
+  files <- files[substr(files,nchar(files)-3, nchar(files)) == ".txt"]
 
   # Open database
-  (dbh <- pitdb_open(db)) != -1 || stop(paste0("Failed to open database: ", db))
-
-  # get initial snapshot of existing files in download folder.
-  prev <- fileSnapshot(path)
-  files <- rownames(prev$info)
+  if ((dbh <- pitdb_open(db)) == -1){
+    logging::logerror("Failed to open database: %s", db)
+    logging::loginfo("Shutting down import server.")
+    return(-1)
+  }
 
   # Get list of already imported files.
   tblImports <- RODBC::sqlFetch(dbh, "tblImports", as.is = T) %>% ensure_data_is_returned
   pitdb_close(dbh)
 
-  # Check if filename in list of imported files with or without full path.
-  new_files <- basename(if (compare_full_pathname) {
-      dplyr::setdiff(file.path(prev$path, files), tblImports$Filename)
+  # return vector of new files
+  basename(if (compare_full_pathname) {
+      dplyr::setdiff(file.path(prev$path, files, fsep = "\\"), tblImports$Filename)
     } else {
       dplyr::setdiff(files, basename(tblImports$Filename))
     }
   )
-
-  # Process new_files (if any), sleep, check for new files, process new files, ad nauseum...
-  while(True) {
-    if (length(new_files) != 0) {
-      print(paste0("Processing new files: ", paste(new_files, collapse = ",")))
-      do_import(files = file.path(prev$path, files), report_path = report_path)
-    }
-
-    # sleep for until next check
-    Sys.sleep(sleep_time)
-
-    # Get a new snapshot and compare
-    curr <- fileSnapshot(path)
-    new_files <- changedFiles(prev, curr)$added
-    prev <- curr
-
-    # keep tidy
-    gc()
- }
 }
-
